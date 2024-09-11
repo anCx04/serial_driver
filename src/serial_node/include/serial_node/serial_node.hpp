36bc475c1665b4a3afdf1d08afc1e962b6ce2822
root <45431466+anCx04@users.noreply.github.com>
2024-09-11 15:41:28 +0200

    Changed the node activation logic

--- src/serial_node/include/serial_node/serial_node.hpp
+++ src/serial_node/include/serial_node/serial_node.hpp
@@ -1,3 +1,12 @@
+/**
+ * ROS 2 Serial Comunication Driver Node implementation
+ *
+ * Andrea Crescenzi <crescenziand@gmail.com>
+ * Roberto Masocco <robmasocco@gmail.com>
+ *
+ * September 11, 2024
+ */
+
 #ifndef SERIAL_NODE_HPP_
 #define SERIAL_NODE_HPP_
 
@@ -10,7 +19,9 @@
 #include <memory>
 #include <thread>
 
-
+/**
+ * Drives SerialNode
+ */
 class SerialNode : public rclcpp::Node
 {
 public:
@@ -18,10 +29,11 @@ public:
    ~SerialNode();
 
 private:
+    /* Node parameters */
     std::string port_name_;
     int baud_rate_;
     std::string terminator_;
-
+    bool verbose_;
     int32_t inter_byte_timeout_ ;
     int32_t read_timeout_constant_ ;
     int32_t read_timeout_multiplier_ ;
@@ -29,23 +41,22 @@ private:
     int32_t write_timeout_multiplier_ ;
     serial::Timeout timeout_ ;
 
-    int32_t sampling_thread_;
-
+    /* Service and Param callbacks */
     rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr enable_service_;
     void enableServiceCallback(const std_srvs::srv::SetBool::Request::SharedPtr request,std_srvs::srv::SetBool::Response::SharedPtr response);
     bool is_enabled_=false;
 
+    /* Service and Param Publisher */
     std::unique_ptr<serial::Serial> serial_port_;
     std::thread serial_read_;
     rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr publisher_;
     void serialReadThread();
 
+    /* Service and Param Subscriber */
     rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr subscriber_;
     void serialInputCallback(const std_msgs::msg::UInt8MultiArray::SharedPtr msg);
 
-    rclcpp::TimerBase::SharedPtr pub_timer_;
-    void pub_timer_callback(void);
-
+    /* Utility routines */
     void removeTerminator(std::string& result, const std::string& terminator);
 
 };
--- src/serial_node/src/serial_node.cpp
+++ src/serial_node/src/serial_node.cpp
@@ -1,10 +1,26 @@
+/**
+ * ROS 2 Serial Comunication Driver Node implementation
+ *
+ * Andrea Crescenzi <crescenziand@gmail.com>
+ * Roberto Masocco <robmasocco@gmail.com>
+ *
+ * September 11, 2024
+ */
+
+
 #include <iostream>
 #include "serial_node/serial_node.hpp"
 
+/**
+ * @brief Builds a new SerialDriverNode.
+ *
+ * @param opts ROS 2 node options.
+ *
+ */
 SerialNode::SerialNode()
 : Node("serial_node")
 {
-  // Leggi i parametri
+  // Declare ROS 2 parameters
   this->declare_parameter<std::string>("port_name", "/dev/ttyUSB0");
   this->declare_parameter<int>("baud_rate", 9600);
   this->declare_parameter<std::string>("terminator", "");
@@ -13,8 +29,9 @@ SerialNode::SerialNode()
   this->declare_parameter<int32_t>("read_timeout_multiplier", 0);
   this->declare_parameter<int32_t>("write_timeout_constant", 1000);
   this->declare_parameter<int32_t>("write_timeout_multiplier", 0);
-  this->declare_parameter<int32_t>("sampling_thread", 300);
+  this->declare_parameter<bool>("verbose",false);
 
+  // Get parameters and assign them to member variables
   this->get_parameter("port_name", port_name_);
   this->get_parameter("baud_rate", baud_rate_);
   this->get_parameter("terminator", terminator_);
@@ -23,179 +40,211 @@ SerialNode::SerialNode()
   this->get_parameter("read_timeout_multiplier", read_timeout_multiplier_);
   this->get_parameter("write_timeout_constant", write_timeout_constant_);
   this->get_parameter("write_timeout_multiplier", write_timeout_multiplier_);
-  this->get_parameter("sampling_thread", sampling_thread_);
+  this->get_parameter("verbose",verbose_);
 
+  // Set timeout parameters for serial communication
   timeout_ = serial::Timeout(
-        (inter_byte_timeout_ == -1) ? serial::Timeout::max() : inter_byte_timeout_,
-        read_timeout_constant_,
-        read_timeout_multiplier_,
-        write_timeout_constant_,
-        write_timeout_multiplier_
-    );
+    (inter_byte_timeout_ == -1) ? serial::Timeout::max() : inter_byte_timeout_,
+    read_timeout_constant_,
+    read_timeout_multiplier_,
+    write_timeout_constant_,
+    write_timeout_multiplier_
+  );
 
+  // Create service to enable/disable the serial node
   enable_service_ =
     this->create_service<std_srvs::srv::SetBool>(
     "/enable",
     std::bind(
       &SerialNode::enableServiceCallback, this, std::placeholders::_1,
       std::placeholders::_2));
-
-
 }
 
+/**
+ * @brief Cleans up stuff upon node termination.
+ */
 SerialNode::~SerialNode()
 {
+  // Clean up the serial read thread if it is still running
   if (serial_read_.joinable()) {
     serial_read_.join();
   }
+  // Close the serial port if it is open
   if (serial_port_ && serial_port_->isOpen()) {
     serial_port_->close();
   }
 }
 
+/**
+ * @brief enables or disables the serial node
+ *
+ * @param request Service request to parse.
+ * @param response Service response to populate.
+ */
 void SerialNode::enableServiceCallback(
   const std_srvs::srv::SetBool::Request::SharedPtr request,
   std_srvs::srv::SetBool::Response::SharedPtr response)
 {
+  // If request is to enable and node is not enabled then I create the pub and sub
   if (request->data && !is_enabled_) {
-    // Abilita il nodo
-
-    RCLCPP_INFO(this->get_logger(), "Port name: %s", port_name_.c_str());
-    RCLCPP_INFO(this->get_logger(), "Baud rate: %d", baud_rate_);
-    RCLCPP_INFO(this->get_logger(), "Terminator: %s", terminator_.c_str());
-
-    //serial_port_ = std::make_unique<serial::Serial>(port_name_, baud_rate_, serial::Timeout::simpleTimeout(1000));
-    serial_port_ = std::make_unique<serial::Serial>(port_name_, baud_rate_,timeout_);
-    if (!serial_port_->isOpen()) {
-      throw std::runtime_error("Failed to open the serial port.");
+    if (verbose_) {
+      RCLCPP_INFO(this->get_logger(), "Port name: %s", port_name_.c_str());
+      RCLCPP_INFO(this->get_logger(), "Baud rate: %d", baud_rate_);
+      RCLCPP_INFO(this->get_logger(), "Terminator: %s", terminator_.c_str());
     }
-    publisher_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>(
-      "/serial_output", rclcpp::QoS(
-        10));
-    subscriber_ = this->create_subscription<std_msgs::msg::UInt8MultiArray>(
-      "/serial_input", rclcpp::QoS(
-        10), std::bind(&SerialNode::serialInputCallback, this, std::placeholders::_1));
+    try {
+      // Open the serial port with the pre-set param
+      serial_port_ = std::make_unique<serial::Serial>(port_name_, baud_rate_, timeout_);
+      if (serial_port_->isOpen()) {
 
-    serial_read_ = std::thread(&SerialNode::serialReadThread, this);
+        // Create publisher for serial output
+        publisher_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>(
+          "/serial_output", rclcpp::QoS(
+            10));
 
-    //pub_timer_ =this->create_wall_timer(std::chrono::milliseconds(300),std::bind(&SerialNode::pub_timer_callback, this));
+        // Create subscriber for serial input
+        subscriber_ = this->create_subscription<std_msgs::msg::UInt8MultiArray>(
+          "/serial_input", rclcpp::QoS(
+            10), std::bind(&SerialNode::serialInputCallback, this, std::placeholders::_1));
 
-    //! Logging macro used to deliver a message to the logging subsystem, INFO level
-    RCLCPP_INFO(this->get_logger(), "Publisher initialized");
+        // Start the serial read thread to cycling execute the port read
+        serial_read_ = std::thread(&SerialNode::serialReadThread, this);
 
+        if (verbose_) {RCLCPP_INFO(this->get_logger(), "Publisher initialized");}
 
-    is_enabled_ = true;
-    response->success = true;
-    response->message = "Node enabled.";
+        // Set the response indicating success
+        is_enabled_ = true;
+        response->success = true;
+        response->message = "";
+      } else {
+        // If failed to open the port
+        response->success = false;
+        response->message = "Failed to open the serial port";
+      }
+    } catch (const serial::IOException & e) {
+      //std::cerr << "Eccezione catturata: " << e.what() << std::endl;
+      response->success = false;
+      //response->message = "Failed to open the serial port";
+      response->message = e.what();
+    }
   } else if (!request->data && is_enabled_) {
-    // Disabilita il nodo
+
+    // If request is to disable and node is enabled
+    // Disable the node, reset the subscriber and publisher
     is_enabled_ = false;
     subscriber_.reset();
+
+    // Join the serial read thread
     if (serial_read_.joinable()) {
       serial_read_.join();
     }
+
+    // Reset publisher and close the serial port
     publisher_.reset();
     serial_port_->close();
     response->success = true;
-    response->message = "Node disabled.";
+    response->message = "";
   } else {
+
     response->success = false;
     response->message = "Node is already in the requested state.";
   }
 }
 
+/**
+ * @brief manages reading from the serial port
+ */
 void SerialNode::serialReadThread()
 {
-  RCLCPP_INFO(this->get_logger(), "Published message %s", "thread ready");
+  if (verbose_) {RCLCPP_INFO(this->get_logger(), "thread ready");}
+
+  // Keep reading from the serial port as long as the node is enabled and running
   while (rclcpp::ok() && is_enabled_) {
-    if (serial_port_->available()) {
-      std::string result;
-      if (!terminator_.empty()) {
-        result = serial_port_->readline(65536, terminator_);
-      } else {
-        result = serial_port_->read(65536);
-      }
+    std::string result;
 
-    /*if (result.size() >= 2 && result[result.size() - 2] == '\r' && result.back() == terminator_.at(0)) {
-        result.erase(result.size() - 2);  // Rimuove gli ultimi due caratteri
+    // Read data from the serial port, either using the terminator or a fixed buffer size
+    if (!terminator_.empty()) {
+      result = serial_port_->readline(65536, terminator_);
+    } else {
+      result = serial_port_->read(65536);
     }
-    else if(result.size() >= 1 && result.back() == terminator_.at(0)){
-        result.erase(result.size() - 1);
-    }*/
-    removeTerminator(result, terminator_);
 
-    auto msg = std_msgs::msg::UInt8MultiArray();
-    msg.data.insert(msg.data.end(), result.begin(), result.end());
-    publisher_->publish(msg);
+    if (result.size() > 0) {
 
+      // Remove the terminator if present
+      removeTerminator(result, terminator_);
+
+      // Create a UInt8MultiArray message and populate it with the received data
+      auto msg = std_msgs::msg::UInt8MultiArray();
+      msg.data.insert(msg.data.end(), result.begin(), result.end());
+
+      // Set the layout for the message (1D array)
+      msg.layout.dim.resize(1);
+      msg.layout.dim[0].label = "byte_array";
+      msg.layout.dim[0].size = result.size();
+      msg.layout.dim[0].stride = result.size();
+
+      publisher_->publish(msg);
+
+      // Log the published data if verbose mode is enabled
+      if (verbose_) {
         std::stringstream ss;
         ss << "Dati da pubblicare: [";
-        for (size_t i = 0; i < msg.data.size(); ++i)
-        {
-            ss << static_cast<int>(msg.data[i]);
-            if (i < msg.data.size()-1)
-            {
-                ss << ", ";
-            }
+        for (size_t i = 0; i < msg.data.size(); ++i) {
+          ss << static_cast<int>(msg.data[i]);
+          if (i < msg.data.size() - 1) {
+            ss << ", ";
+          }
         }
         ss << "]";
         RCLCPP_INFO(this->get_logger(), ss.str().c_str());
-
-      /*std::string message = result;
-      RCLCPP_INFO(this->get_logger(), "Published message %s", message.c_str());*/
+      }
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(sampling_thread_));
+
   }
 }
 
+/**
+ * @brief Callback for receiving serial input and sending it to the serial port
+ *
+ * @param msg string to send.
+ */
 void SerialNode::serialInputCallback(const std_msgs::msg::UInt8MultiArray::SharedPtr msg)
 {
 
+  // Convert the received data into a string and append the terminator if present
   std::string data(msg->data.begin(), msg->data.end());
   if (!terminator_.empty()) {
     data += terminator_;
   }
-  //RCLCPP_INFO(this->get_logger(),data.c_str());
+
   serial_port_->write(data);
 }
 
-void SerialNode::pub_timer_callback(void)
+/**
+ * @brief Function to remove the terminator from the string
+ *
+ * @param result string to be reduced
+ * @param terminator terminator to remove
+ */
+void SerialNode::removeTerminator(std::string & result, const std::string & terminator)
 {
-  // Build the new message
-  std::string new_data = "Hello ";
-  new_data.append(port_name_ + "/" + std::to_string(baud_rate_) + "/" + terminator_ + ".");
+  size_t terminator_length = terminator.size();
 
-  //! Create a new message of the specific interface type
-  //! It is better to initialize it as empty as below
-  std_msgs::msg::String new_msg{};
-
-  //! Populate the data field of the message using its setter method
-  new_msg.set__data(new_data);
-
-  //! Publish the new message by calling the publish method of the publisher member
-  //publisher_->publish(new_msg);
-
-  // Log something
-  std::string message = port_name_ + "/" + std::to_string(baud_rate_) + "/" + terminator_ + ".";
-  RCLCPP_INFO(this->get_logger(), "Published message %s", message.c_str());
-}
-
-void SerialNode::removeTerminator(std::string& result, const std::string& terminator) {
-    size_t terminator_length = terminator.size();
-
-    // Verifica se la lunghezza della stringa è sufficiente per contenere il terminatore
-    if (result.size() >= terminator_length) {
-        // Controlla se la parte finale della stringa corrisponde al terminatore
-        if (result.compare(result.size() - terminator_length, terminator_length, terminator) == 0) {
-            result.erase(result.size() - terminator_length);  // Rimuove il terminatore
-        }
+  // If the result has enough characters to include the terminator
+  if (result.size() >= terminator_length) {
+    // Check if the result ends with the terminator, and remove it
+    if (result.compare(result.size() - terminator_length, terminator_length, terminator) == 0) {
+      result.erase(result.size() - terminator_length);
     }
+  }
 }
 
+// Main function to initialize and run the ROS 2 node
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<SerialNode>());
-  rclcpp::shutdown();
+  rclcpp::init(argc, argv);                           // Initialize ROS 2
+  rclcpp::spin(std::make_shared<SerialNode>());       // Run the SerialNode
+  rclcpp::shutdown();                                 // Shutdown ROS 2
   return 0;
 }
